ad8dad3abb0c86475d4d93d0c40dd586a33d4775
JaiderSAN53 <jsadvincula@unipacifico.edu.co>
2025-09-06 11:45:27 -0500

    Intento de subidad

---
+++ hola.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+ printf("Hola, Jaider usted va ser un programador exitoso\n");
+ return 0;
+}
+    
---
+++ mate.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+ printf("Hola, te gustaria la matematica\n");
+ return 0;
+}
